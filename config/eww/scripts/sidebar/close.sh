6e2815975882991644f09dfd5b51b2d90b141bf8
viandoxdev <titouangoulois29@gmail.com>
2022-02-03 21:05:51 +0100

    some work on second sidebar page

--- config/eww/scripts/sidebar/close.sh
+++ config/eww/scripts/sidebar/close.sh
@@ -2,5 +2,7 @@
 (
 eww update sidebar-revealed=false
 sleep 0.25
+# auto close this
+eww update gh-reload-pfp=false
 eww close sidebar
 ) &
---
+++ config/eww/sh/github.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+user="$(~/dots/inf.sh github_graphql_user)"
+token="$(~/dots/inf.sh github_graphql_token)"
+
+req() {
+	curl -s -u "$user":"$token" -X POST \
+		-H "Content-Type: application/json" \
+		-d '{"query": "'"$(xargs < <(cat -))"'"}' \
+		https://api.github.com/graphql
+}
+
+
+run() {
+	case "$1" in
+		"avatar")
+			if ! [ -f ~/dots/github_pfp.png ]; then
+				curl -s "$({ req <<- END
+					query {
+						viewer {
+							avatarUrl(size: 512)
+						}
+					}
+					END
+				} | jq '.data.viewer.avatarUrl' -r)" -o ~/dots/github_pfp.png
+			fi
+			echo "$HOME/dots/github_pfp.png"
+			;;
+		"user_info")
+			{ req <<- END
+				query {
+	 				viewer {
+		 				name
+						login
+						email
+		 			}
+	 			}
+				END
+			} | jq '.data.viewer'
+			;;
+		"rate")
+				 { req <<- END
+				 	query {
+		 				rateLimit {
+						  limit
+						  remaining
+						  resetAt
+						}
+		 			}
+					END
+				 } | jq '.data.rateLimit'
+				 ;;
+		*)
+			;;
+	esac
+}
+
+[ -z "$1" ] && return 1
+
+if [ "$1" = "--raw" ] || [ "$1" = "-r" ]; then
+		  [ -z "$2" ] && return 1
+		  # disgusting
+		  run "$2" | jq -r 
+elif [ "$2" = "--raw" ] || [ "$2" = "-r" ]; then
+		  run "$1" | jq -r
+else
+		  run "$1" 
+fi
+
+# vi: set ts=3
