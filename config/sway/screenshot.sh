74b49e75c71227dae61a8c2c6a206e08d9158c4b
viandoxdev <titouangoulois29@gmail.com>
2022-02-18 21:40:38 +0100

    screenshot change and more volume changes

--- config/sway/screenshot.sh
+++ config/sway/screenshot.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-
-grim -g "$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp -B ffffff11 -b 00000000 -c ffffffaa -s ffffff11 -w 1)" - | tee ~/pictures/screenshots/$(date +'%F.%k.%M.%S.%N.png') | wl-copy
+if sel="$(
+	swaymsg -t get_tree | \
+	jq -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | \
+	slurp -B ffffff11 -b 00000000 -c ffffffaa -s ffffff11 -w 1
+)"; then
+	grim -g "$sel" - | tee ~/pictures/screenshots/"$(date +'%F.%k.%M.%S.%N.png')" | wl-copy
+fi
